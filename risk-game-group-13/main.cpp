2e7f6a11fdc27fc344075110479a3b6e541a31f9
Bruno Robert <bruno.jeanluke@gmail.com>
2017-09-28 20:36:05 -0400

    Initial Commit

---
+++ risk-game-group-13/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  risk-game-group-13
+//
+//  Created by Bruno Robert on 28/09/2017.
+//  Copyright © 2017 Bruno Robert. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
