ac1e6995f259925d229fa93278f703b7da1b5511
Bruno Robert <bruno.jeanluke@gmail.com>
2017-10-03 17:00:18 -0400

    Initial Commit

---
+++ main game loop/GameLoop.cpp
@@ -0,0 +1,9 @@
+//
+//  GameLoop.cpp
+//  main game loop
+//
+//  Created by Bruno Robert on 03/10/2017.
+//  Copyright © 2017 Bruno Robert. All rights reserved.
+//
+
+#include "GameLoop.h"
---
+++ main game loop/GameLoop.h
@@ -0,0 +1,14 @@
+//
+//  GameLoop.hpp
+//  main game loop
+//
+//  Created by Bruno Robert on 03/10/2017.
+//  Copyright © 2017 Bruno Robert. All rights reserved.
+//
+
+#ifndef GameLoop_hpp
+#define GameLoop_hpp
+
+#include <stdio.h>
+
+#endif /* GameLoop_hpp */
