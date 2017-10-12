92486a1d88446f8d7fefb43d5a252c6dd3807f95
Bruno Robert <bruno.jeanluke@gmail.com>
2017-10-12 10:35:45 -0400

    Creation of fortification phase class

---
+++ Assignment 2 - part 6 - main game loop: fortification phase/FortificationPhase.cpp
@@ -0,0 +1,9 @@
+//
+//  FortificationPhase.cpp
+//  Assignment 2 - part 6 - main game loop: fortification phase
+//
+//  Created by Bruno Robert on 12/10/2017.
+//  Copyright © 2017 Bruno Robert. All rights reserved.
+//
+
+#include "FortificationPhase.hpp"
---
+++ Assignment 2 - part 6 - main game loop: fortification phase/FortificationPhase.hpp
@@ -0,0 +1,14 @@
+//
+//  FortificationPhase.hpp
+//  Assignment 2 - part 6 - main game loop: fortification phase
+//
+//  Created by Bruno Robert on 12/10/2017.
+//  Copyright © 2017 Bruno Robert. All rights reserved.
+//
+
+#ifndef FortificationPhase_hpp
+#define FortificationPhase_hpp
+
+#include <stdio.h>
+
+#endif /* FortificationPhase_hpp */
