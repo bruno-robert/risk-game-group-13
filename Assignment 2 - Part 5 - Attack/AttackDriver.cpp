a41445fae37f17b2a8f8bc57d9cc7c13209a4507
Sabrina-R <32601139+Sabrina-R@users.noreply.github.com>
2017-10-24 20:06:33 -0400

    Added commented header

--- Assignment 2 - Part 5 - Attack/AttackDriver.cpp
+++ Assignment 2 - Part 5 - Attack/AttackDriver.cpp
@@ -1,3 +1,11 @@
+//============================================================================
+// Name        : AttackDriver.cpp
+// Team        : Group 13
+// Team members: Bruno Robert 40003443, C Jeffrey Maher 40018878, Sabrina Rieck 40032864, Simon Roy 40030996
+// Version     : 1
+// Copyright   : GNU Public license 3
+// Description : Tests that Attack.h is working correctly
+//============================================================================
 
 #include <iostream>
 #include "Attack.h"
