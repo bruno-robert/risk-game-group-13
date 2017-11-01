ba4f246dc1c3451e0e84643de20a440ae5e0ab0a
Bruno Robert <bruno.jeanluke@gmail.com>
2017-11-01 17:00:40 -0400

    Creating Abstract Class to be parent of the PSP's

--- Assignment 3/Part 1 - Player Strategy Pattern/PSPatern.hpp
+++ Assignment 3/Part 1 - Player Strategy Pattern/PSPatern.hpp
@@ -10,15 +10,23 @@
 #define PSPatern_hpp
 
 #include <stdio.h>
+#include <vector>
+
+using namespace std;
 //this is to be the parent class of Human Agressive and Benevolent
 class PlayerStrategyPattern {
 public:
     PlayerStrategyPattern();
     virtual ~PlayerStrategyPattern();
+    virtual void getFortifyValues(int * startingCountry, int * destinationcountry, int * numberOftroupsToMove)= 0;
+    virtual void getAttackValues()= 0;
+    virtual void getReinforceValues()= 0;
 };
 //subclass of PlayerStrategyPattern
 class Human : public PlayerStrategyPattern{
 public:
+    //Takes human input to changes the values of the given variables pointed to pointers. Before returning the function verifies that the given values are valid.
+    void getFortifyValues(int * startingCountry, int * destinationcountry, int * numberOftroupsToMove);
     
 };
 //subclass of PlayerStrategyPattern
