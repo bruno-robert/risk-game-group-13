85ba3e1239821e4dcb655a95df535cf24165dbed
Bruno Robert <bruno.jeanluke@gmail.com>
2017-11-02 10:57:08 -0400

    added helper method: countryNameToID implemented helper method: countryNameToID
    continued implementation of Human class

--- Assignment 3/Part 1 - Player Strategy Pattern/PSPatern.cpp
+++ Assignment 3/Part 1 - Player Strategy Pattern/PSPatern.cpp
@@ -7,3 +7,59 @@
 //
 
 #include "PSPatern.hpp"
+using namespace std;
+
+Human::Human() {
+    
+}
+
+Human::~Human() {
+    
+}
+
+/**
+ Takes a string and a Map and returns the Id of the country with the given name
+ If the country doesn't exists, retursn -1
+ @param countryName Name of the country
+ @param m Map Object
+ @return the ID of the country with name countryName. If the country doesn't exist, returns -1
+ */
+int countryNameToID(string countryName, Map& m){
+    vector<CountryNode*> cl = m.getCountryList();
+    for (vector<CountryNode*>::iterator country = cl.begin(); country != cl.end(); country++) {
+        if((*country)->getCountName() == countryName) {
+            return (*country)->getCountryId();
+        }
+    }
+    
+    return -1;//error, country not found
+}
+
+void Human::getFortifyValues(int *startingCountry, int *destinationCountry, int *numberOftroupsToMove, Map& m) {
+    
+    
+    //Getting and checking starting country
+    cout << "please select a starting country" << endl;
+    while(true) {
+        cin >> *startingCountry;
+        
+        //checks if the starting coutry is valid
+        if((*startingCountry >= 1) && (m.getcoutryById(*startingCountry) != NULL)) {
+            break;
+        }
+        
+        cout << "Sorry, the value you entered is not valid, please try again:" << endl;
+    }
+
+    //Getting and checking the destination country
+    cout << "please select the destination country" << endl;
+    while(true) {
+        cin >> *destinationCountry;
+        if((*destinationCountry >= 1) && (m.getcoutryById(*destinationCountry) != NULL)) {
+            break;
+        }
+        
+        cout << "Sorry, the value you entered is not valid, please try again:" << endl;
+    }
+    
+}
--- Assignment 3/Part 1 - Player Strategy Pattern/PSPatern.hpp
+++ Assignment 3/Part 1 - Player Strategy Pattern/PSPatern.hpp
@@ -11,22 +11,30 @@
 
 #include <stdio.h>
 #include <vector>
+#include <iostream>
 
+#include "Map.h"
 using namespace std;
 //this is to be the parent class of Human Agressive and Benevolent
 class PlayerStrategyPattern {
 public:
     PlayerStrategyPattern();
     virtual ~PlayerStrategyPattern();
-    virtual void getFortifyValues(int * startingCountry, int * destinationcountry, int * numberOftroupsToMove)= 0;
+    virtual void getFortifyValues(int * startingCountry, int * destinationCountry, int * numberOftroupsToMove, Map& m)= 0;
     virtual void getAttackValues()= 0;
     virtual void getReinforceValues()= 0;
 };
 //subclass of PlayerStrategyPattern
 class Human : public PlayerStrategyPattern{
+    int countryNameToID(string countryName);
 public:
+    
+    Human();
+    ~Human();
     //Takes human input to changes the values of the given variables pointed to pointers. Before returning the function verifies that the given values are valid.
-    void getFortifyValues(int * startingCountry, int * destinationcountry, int * numberOftroupsToMove);
+    void getFortifyValues(int * startingCountry, int * destinationCountry, int * numberOftroupsToMove, Map& m);
+    void getAttackValues();
+    void getReinforceValues();
     
 };
 //subclass of PlayerStrategyPattern
