1ffe10826d1629a7dabc24c12bfec197aa068394
Bruno Robert <bruno.jeanluke@gmail.com>
2017-11-07 22:10:42 -0500

    added A player for each PSP and initialised them

--- Assignment 3/Part 1 - Player Strategy Pattern/AggroDriver.cpp
+++ Assignment 3/Part 1 - Player Strategy Pattern/AggroDriver.cpp
@@ -46,26 +46,44 @@ int main(){
 	hand1.add(Card(1));
 	hand1.add(Card(2));
 	hand1.add(Card(3));
+    
+    Hand hand2;
+    hand2.add(Card(1));
+    hand2.add(Card(2));
+    hand2.add(Card(3));
+    
+    Hand hand3;
+    hand3.add(Card(1));
+    hand3.add(Card(2));
+    hand3.add(Card(3));
+    
 
 	//Creating a dice object
 
 	Dice dice1;
+    Dice dice2;
+    Dice dice3;
 
 	//Creating a player object
 
 	//Aggressive strat;
 
-	PlayerStrategyPattern* strat = new Aggressive();
+	PlayerStrategyPattern* aggressiveStrat = new Aggressive();
+    PlayerStrategyPattern* humanStrat = new Human();
+    PlayerStrategyPattern* benevolentStrat = new Benevolant();
 
-	Player player1 = Player(player1Set, hand1, dice1, strat);
-	player1.setPlayerID(1);
+	Player player1 = Player(player1Set, hand1, dice1, aggressiveStrat);
+    Player player2 = Player(player1Set, hand2, dice2, humanStrat);
+    Player player3 = Player(player1Set, hand3, dice3, benevolentStrat);
+    
+    
 
 	cout << "\nPlayer 1's list of countries and their units: " << endl;
 	for (int i = 0; i < player1.getCountry().size(); i++) {
 		cout << player1.getCountry().at(i)->getCountName() << " has " << player1.getCountry().at(i)->getNumberOfTroops() << " units." << endl;
 	}
 
-	strat->executeReinforce(player1);
+	aggressiveStrat->executeReinforce(player1);
 
 	cout << "\nPlayer 1's list of countries and their units: " << endl;
 	for (int i = 0; i < player1.getCountry().size(); i++) {
