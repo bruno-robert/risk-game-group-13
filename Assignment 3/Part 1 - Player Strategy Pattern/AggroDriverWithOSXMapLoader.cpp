342684b312a7cd9dd46a4ee3c2a20721075e937a
Bruno Robert <bruno.jeanluke@gmail.com>
2017-11-07 23:52:10 -0500

    Updated OSX PSP Driver

--- Assignment 3/Part 1 - Player Strategy Pattern/AggroDriverWithOSXMapLoader.cpp
+++ Assignment 3/Part 1 - Player Strategy Pattern/AggroDriverWithOSXMapLoader.cpp
@@ -1,43 +1,44 @@
 
 #include "PSPatern.hpp"
 #include "Player.h"
+#include "MapLoaderOSX.h"
 
 int main(){
 
-	//Creating 2 ContinentNodes
-
-	ContinentNode europe("Europe", 5);
-	ContinentNode asia("Asia", 3);
-
-	//Creating a few CountryNodes
-
-	CountryNode france("France", &europe);
-	CountryNode uk("U.K.", &europe);
-	CountryNode spain("Spain", &europe);
-	CountryNode china("China", &asia);
-	CountryNode mongolia("Mongolia", &asia);
-
-	france.setNumberOfTroops(7);
-	uk.setNumberOfTroops(5);
-	spain.setNumberOfTroops(3);
-	china.setNumberOfTroops(2);
-	mongolia.setNumberOfTroops(8);
-
-	china.setOwnedBy(2);
-	mongolia.setOwnedBy(2);
-
-	uk.addAdjCount(&china);
-	uk.addAdjCount(&mongolia);
-	spain.addAdjCount(&mongolia);
-
-
-
+    MapLoader ml;
+    string value = "World.map";
+    ml.ReadFile(value);
+    ml.printMap();
+    Map *m = ml.getMap();
 	//Creating a vector of CountryNode pointers for my player
-
+    
+    int ctr = 1;
+    vector<CountryNode*> countryList = m->getCountryList();
 	vector<CountryNode*> player1Set;
-	player1Set.push_back(&france);
-	player1Set.push_back(&spain);
-	player1Set.push_back(&uk);
+    vector<CountryNode*> player2Set;
+    vector<CountryNode*> player3Set;
+    for(vector<CountryNode*>::iterator country = countryList.begin(); country != countryList.end(); country++) {
+        (*country)->setNumberOfTroops(0);
+        switch (ctr) {
+            case 1:
+                player1Set.push_back(*country);
+                break;
+            case 2:
+                player2Set.push_back(*country);
+                break;
+            case 3:
+                player3Set.push_back(*country);
+                break;
+            default:
+                break;
+        }
+        
+        
+        ctr++;
+        if(ctr > 3) {
+            ctr = 1;
+        }
+    }
 	
 
 	//Creating a hand object
@@ -73,8 +74,9 @@ int main(){
     PlayerStrategyPattern* benevolentStrat = new Benevolant();
 
 	Player player1 = Player(player1Set, hand1, dice1, aggressiveStrat);
-    Player player2 = Player(player1Set, hand2, dice2, humanStrat);
-    Player player3 = Player(player1Set, hand3, dice3, benevolentStrat);
+    Player player2 = Player(player2Set, hand2, dice2, humanStrat);
+    Player player3 = Player(player3Set, hand3, dice3, benevolentStrat);
+    
     
     
 
@@ -82,13 +84,21 @@ int main(){
 	for (int i = 0; i < player1.getCountry().size(); i++) {
 		cout << player1.getCountry().at(i)->getCountName() << " has " << player1.getCountry().at(i)->getNumberOfTroops() << " units." << endl;
 	}
+    cout << "\nPlayer 2's list of countries and their units: " << endl;
+    for (int i = 0; i < player2.getCountry().size(); i++) {
+        cout << player2.getCountry().at(i)->getCountName() << " has " << player2.getCountry().at(i)->getNumberOfTroops() << " units." << endl;
+    }
+    cout << "\nPlayer 3's list of countries and their units: " << endl;
+    for (int i = 0; i < player3.getCountry().size(); i++) {
+        cout << player3.getCountry().at(i)->getCountName() << " has " << player3.getCountry().at(i)->getNumberOfTroops() << " units." << endl;
+    }
 
-	aggressiveStrat->executeReinforce(player1);
+//    aggressiveStrat->executeReinforce(player1);
 
-	cout << "\nPlayer 1's list of countries and their units: " << endl;
-	for (int i = 0; i < player1.getCountry().size(); i++) {
-		cout << player1.getCountry().at(i)->getCountName() << " has " << player1.getCountry().at(i)->getNumberOfTroops() << " units." << endl;
-	}
+//    cout << "\nPlayer 1's list of countries and their units: " << endl;
+//    for (int i = 0; i < player1.getCountry().size(); i++) {
+//        cout << player1.getCountry().at(i)->getCountName() << " has " << player1.getCountry().at(i)->getNumberOfTroops() << " units." << endl;
+//    }
 
     return 0;
 }
