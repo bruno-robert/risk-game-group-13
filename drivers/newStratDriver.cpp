bcdd1528398f6f380778129dfb045243e5e7c57b
Bruno Robert <bruno.jeanluke@gmail.com>
2017-11-16 21:21:27 -0500

    added newStratDriver

---
+++ drivers/newStratDriver.cpp
@@ -0,0 +1,15 @@
+//
+//  newStratDriver.cpp
+//  newStrats
+//
+//  Created by Bruno Robert on 16/11/2017.
+//  Copyright © 2017 Bruno Robert. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
