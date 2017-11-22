8c49d4c7dfaf8946b39c1b318f5274e136b56eb6
Bruno Robert <bruno.jeanluke@gmail.com>
2017-11-22 11:50:37 -0500

    reorganising the Methods from different classes (for ease of reading and finding them)

--- src/PSPatern.cpp
+++ src/PSPatern.cpp
@@ -11,6 +11,10 @@
 #include "Subject.h"
 using namespace std;
 
+//---------------------
+// Parent PSP
+//---------------------
+
 PlayerStrategyPattern::PlayerStrategyPattern() {
 
 }
@@ -34,6 +38,62 @@ bool PlayerStrategyPattern::isCountryInVector(int a, vector<CountryNode*> vect) 
 	return false;
 }
 
+/**
+ the recursiveGetPathToBiggest country starts from the the startingCountry, and makes its way to the nearest country that is owned by p and has troops
+ It then returns a 1 length path to move troops cloesr to the original starting country
+ */
+vector<CountryNode*> PlayerStrategyPattern::recursiveGetPathToBiggest(CountryNode* startingCountry, CountryNode* destinationCountry, const Player& p, vector<CountryNode*>& visitedCountries) {
+    vector<CountryNode*> path;
+    if (isCountryInVector(startingCountry->getCountryId(), visitedCountries)) {
+        return path;
+    }
+    else {
+        visitedCountries.push_back(startingCountry);
+        for (CountryNode* adjCountry : startingCountry->getAdjCount()) {
+            if (adjCountry->getOwnedBy() == p.getPlayerID() && adjCountry->getNumberOfTroops() > 1) {
+                destinationCountry = adjCountry;
+                path.push_back(startingCountry);
+                path.push_back(destinationCountry);
+                return path;
+            }
+        }
+        for (CountryNode* adjCountry : startingCountry->getAdjCount()) {
+            path = recursiveGetPathToBiggest(adjCountry, destinationCountry, p, visitedCountries);
+            if (path.size() > 0) {
+                return path;
+            }
+        }
+    }
+    
+    return path;
+}
+
+/**
+ 
+ @param destinationCountry ptr to the coutry to witch you want to move troups (the method will modify it's value so that if you move troop to this country, they will be closer to the original country)
+ @param startingCountry the method will change this to the country you should send the troups from
+ @param p player
+ */
+void PlayerStrategyPattern::getPathToBiggest(CountryNode ** destinationCountry, CountryNode ** startingCountry, const Player& p) {
+    vector<CountryNode*> visitedCountries;
+    vector<CountryNode*> path = recursiveGetPathToBiggest(*destinationCountry, *startingCountry, p, visitedCountries);
+    
+    if (path.size() >= 2) {
+        *destinationCountry = path.at(0);
+        *startingCountry = path.at(1);
+    }
+    else {
+        *destinationCountry = NULL;
+        *startingCountry = NULL;
+    }
+    
+    
+    return;
+}
+
+//---------------------
+// Human
+//---------------------
 
 Human::Human() : PlayerStrategyPattern() {
 
@@ -198,6 +258,10 @@ void Human::executeReinforce(Player& user, Reinforce& rein) {
 	rein.reinforceDistributions(user);
 }
 
+//---------------------
+// Agressive
+//---------------------
+
 Aggressive::Aggressive() : PlayerStrategyPattern()
 {
 
@@ -358,58 +422,7 @@ void Aggressive::executeAttack(Player& user, Map& map, vector<Player*> playerLis
 }
 
 
-/**
- the recursiveGetPathToBiggest country starts from the the startingCountry, and makes its way to the nearest country that is owned by p and has troops
- It then returns a 1 length path to move troops cloesr to the original starting country
- */
-vector<CountryNode*> PlayerStrategyPattern::recursiveGetPathToBiggest(CountryNode* startingCountry, CountryNode* destinationCountry, const Player& p, vector<CountryNode*>& visitedCountries) {
-	vector<CountryNode*> path;
-	if (isCountryInVector(startingCountry->getCountryId(), visitedCountries)) {
-		return path;
-	}
-	else {
-		visitedCountries.push_back(startingCountry);
-		for (CountryNode* adjCountry : startingCountry->getAdjCount()) {
-			if (adjCountry->getOwnedBy() == p.getPlayerID() && adjCountry->getNumberOfTroops() > 1) {
-				destinationCountry = adjCountry;
-				path.push_back(startingCountry);
-				path.push_back(destinationCountry);
-				return path;
-			}
-		}
-		for (CountryNode* adjCountry : startingCountry->getAdjCount()) {
-			path = recursiveGetPathToBiggest(adjCountry, destinationCountry, p, visitedCountries);
-			if (path.size() > 0) {
-				return path;
-			}
-		}
-	}
 
-	return path;
-}
-
-/**
-
- @param destinationCountry ptr to the coutry to witch you want to move troups (the method will modify it's value so that if you move troop to this country, they will be closer to the original country)
- @param startingCountry the method will change this to the country you should send the troups from
- @param p player
- */
-void PlayerStrategyPattern::getPathToBiggest(CountryNode ** destinationCountry, CountryNode ** startingCountry, const Player& p) {
-	vector<CountryNode*> visitedCountries;
-	vector<CountryNode*> path = recursiveGetPathToBiggest(*destinationCountry, *startingCountry, p, visitedCountries);
-
-	if (path.size() >= 2) {
-		*destinationCountry = path.at(0);
-		*startingCountry = path.at(1);
-	}
-	else {
-		*destinationCountry = NULL;
-		*startingCountry = NULL;
-	}
-
-
-	return;
-}
 
 void Aggressive::executeFortify(Player& user, FortificationPhase& fortification) {
 	CountryNode* startingCountry = NULL;
@@ -467,6 +480,10 @@ void Aggressive::executeFortify(Player& user, FortificationPhase& fortification)
 	}
 
 }
+
+//---------------------
+// Benevolent
+//---------------------
 
 
 Benevolant::Benevolant() : PlayerStrategyPattern()
